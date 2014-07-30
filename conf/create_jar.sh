a5d239b24fc855b2af9d21aebd34d89932fcbbe1
chhaviganwgal <chhavi.ganwgal@impetus.co.in>
2014-07-30 12:22:38 +0530

    java home

--- conf/create_jar.sh
+++ conf/create_jar.sh
@@ -2,10 +2,10 @@
 cd
 cd $1
 if [ $3 = 'mongodb' ] ; then 
-/usr/local/java/jdk1.7.0_45/bin/jar cvf dynamic-mongodb-entity.jar $2
+$JAVA_HOME/bin/jar cvf dynamic-mongodb-entity.jar $2
 else 
 if [ $3 = 'cassandra' ] ; then 
-/usr/local/java/jdk1.7.0_45/bin/jar cvf dynamic-cassandra-entity.jar $2
+$JAVA_HOME/bin/jar cvf dynamic-cassandra-entity.jar $2
 else
 echo "none"
 fi
